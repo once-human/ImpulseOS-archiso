bbbecc2a7da563ac9e30e8cf7d81c21c02da062d
Onkar Yaglewad <121476278+once-human@users.noreply.github.com>
2024-09-23 11:08:59 +0530

    Post Install Dotfiles Customization

---
+++ configs/releng/airootfs/usr/local/bin/post_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run Illogical Impulse dotfiles setup script
+bash <(curl -s "https://end-4.github.io/dots-hyprland-wiki/setup.sh")
+
+# Get the currently logged-in user's home directory
+USER_HOME=$(eval echo ~${SUDO_USER})
+
+# Force replace the hypr directory with the customized one
+cp -rf /etc/skel/.config/hypr "$USER_HOME/.config/"
+
+# Ensure ownership and permissions are correct
+chown -R ${SUDO_USER}:${SUDO_USER} "$USER_HOME/.config/hypr"
